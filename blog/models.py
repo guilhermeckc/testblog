4dc41a7180f1ed4ef0dea64027a61ff737c7ee80
Guilherme Cezar <guilherme_ckc@hotmail.com>
2020-12-10 20:56:47 -0300

    Start project with 1st app and model

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    microorganism = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
